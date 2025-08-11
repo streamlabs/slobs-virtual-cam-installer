3735256367bf71fdbe289e817cd3bd5cf7df79c2
Richard Osborne <rosborne255@yahoo.com>
2025-08-10 19:10:31 -0500

    check for env var before replacing profile

--- codesign_development.sh
+++ codesign_development.sh
@@ -3,7 +3,11 @@ cd build/RelWithDebInfo
 
 /usr/bin/codesign -o runtime --force --sign "$APPLE_SLD_IDENTITY" --entitlements "$root"/build/camera-ext-entitlements.plist  --timestamp\=none --generate-entitlement-der slobs-virtual-cam-installer.app/Contents/Library/SystemExtensions/com.streamlabs.slobs.mac-camera-extension.systemextension
 
-cp -R "$SYSEXT_INSTALLER_PROVISIONING_PROFILE" slobs-virtual-cam-installer.app/Contents/embedded.provisionprofile
+if [ -n "$MY_VAR" ]; then
+  echo "$0 replace provisioning profile"
+  cp -R "$SYSEXT_INSTALLER_PROVISIONING_PROFILE" slobs-virtual-cam-installer.app/Contents/embedded.provisionprofile
+fi
+
 /usr/bin/codesign -o runtime --force --sign "$APPLE_SLD_IDENTITY" --entitlements "$root"/entitlements.plist --timestamp\=none --generate-entitlement-der /Users/rosbo/projects/streamlabs/slobs-vcam-installer/build/RelWithDebInfo/slobs-virtual-cam-installer.app
 
 echo "$0 run ditto slobs-virtual-cam-installer.app /Applications/slobs-virtual-cam-installer.app"
